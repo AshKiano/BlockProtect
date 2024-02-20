6678651d2800a79b81973d3df5451723197c1341
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-02-20 22:31:43 +0100

    version 1.5

--- src/main/java/com/ashkiano/blockprotect/BlockProtect.java
+++ src/main/java/com/ashkiano/blockprotect/BlockProtect.java
@@ -4,7 +4,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class BlockProtect extends JavaPlugin implements Listener {
@@ -21,6 +26,8 @@ public class BlockProtect extends JavaPlugin implements Listener {
         Metrics metrics = new Metrics(this, 19302);
 
         this.getLogger().info("Thank you for using the BlockProtect plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
+
+        checkForUpdates();
     }
 
     @EventHandler
@@ -28,6 +35,46 @@ public class BlockProtect extends JavaPlugin implements Listener {
         if (protectedBlocks.contains(event.getBlock().getType().toString())) {
             event.setCancelled(true);
             event.getPlayer().sendMessage("You cannot break this block!");
+        }
+    }
+
+    private void checkForUpdates() {
+        try {
+            String pluginName = this.getDescription().getName();
+            URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                if (jsonObject.has("error")) {
+                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                } else {
+                    String latestVersion = jsonObject.getString("latest_version");
+
+                    String currentVersion = this.getDescription().getVersion();
+                    if (currentVersion.equals(latestVersion)) {
+                        this.getLogger().info("This plugin is up to date!");
+                    } else {
+                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    }
+                }
+            } else {
+                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
         }
     }
 }
